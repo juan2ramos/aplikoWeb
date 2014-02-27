e5220900eca786d68ec05fe92669ad8e3dfa654c
juan2ramos <juan2ramos@gmail.com>
2014-02-27 18:05:15 -0500

    estructura html

--- index.php
+++ index.php
@@ -27,6 +27,217 @@
 
     </head>
     <body>
+        <header>
+            <div id="social">
+                <h2>Fabricamos apps para smartphones, tablets y computadores</h2>
+                <ul>
+                    <li>f</li>
+                    <li>t</li>
+                    <li>y</li>
+                </ul>
+            </div>
+            <nav class="">
+                <figure>
+                    <img src="images" alt="logo-apliko">
+                </figure>
+                <ul>
+                    <li><a href="#"></a></li><!--
+                 --><li><a href="#"></a></li><!--
+                 --><li><a href="#"></a></li><!--
+                 --><li><a href="#"></a></li>
+                </ul>
+            </nav>
+            <div id="entities-contend-head">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div id="banner">
+                
+            </div>
+            <div id="members">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+        </header>
+        <section id="entities-contend">
+
+            <figure id="lens">
+                <img src="" alt="">
+            </figure>
+            <div id="contend-article">
+                <article>
+                    <figure>
+                        <img src="" alt="aplico para personas">
+                    </figure>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <a href="">Conoce más</a>
+                     
+                </article>
+                <article>
+                    <figure>
+                        <img src="" alt="aplico para empresas">
+                    </figure>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <a href="">Conoce más</a>
+                     
+                </article>
+    
+            </div>
+        </section>
+        <section id="about">
+            <article>
+                <figure>
+                    <img src="" alt="esto es aplico">
+                    
+                </figure>
+                <figure>
+                    <img src="" alt="infograma apliko">
+                </figure>
+            </article>
+            <article>
+                
+                <figure>
+                    <img src="" alt="Esto hacemos">
+                </figure>
+                <div id="do-first">
+                    <ol>
+                        <li>
+                            <h2>asesoramos</h2>
+                            <p></p>
+                            <figure>
+                                <img src="" alt="Asesoramos">
+                            </figure>
+                        </li>
+                        <li>
+                            <h2>diseñamos uso</h2>
+                            <p></p>
+                            <figure>
+                                <img src="" alt="Uso">
+                            </figure>
+                        </li>
+                        <li>
+                            <h2>diseñamos apariecia</h2>
+                            <p></p>
+                            <figure>
+                                <img src="" alt="Apariencia">
+                            </figure>
+                        </li>
+                        
+                    </ol>
+                </div>
+                <div id="do-second">
+                    <ol>
+                        <li>
+                            <figure>
+                                <img src="" alt="Desarrollamos">
+                            </figure>
+                            <h2>desarrollamos</h2>
+                            <p></p>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="Activamos">
+                            </figure>
+                            <h2>activamos</h2>
+                            <p></p>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="Promocionamos">
+                            </figure>
+                            <h2>promocionamos</h2>
+                            <p></p>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="" alt="Gestionamos">
+                            </figure>
+                            <h2>gestionamos</h2>
+                            <p></p>
+                        </li>
+                        
+                    </ol>
+                </div>
+            </article>
+            <article>
+                <h2>Creamos 2 clases de apps</h2>
+                <div class="apps">
+                    <h3>Apps Nativas</h3>
+                    <p></p>
+                    <figure>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </figure>
+                </div>
+                <div class="apps">
+                    <h3>Web Apps</h3>
+                    <p></p>
+                    <figure>
+                        <img src="" alt="">
+                    </figure>
+                </div>
+            </article>
+        </section>  
+        <section id="our-apps">
+            
+        </section> 
+        <section id="people">
+            <figure>
+                <img src="" alt="para personas">
+            </figure>
+            <article>
+                
+                <h2>Comparte tus ideas y gana con nosotros</h2>
+                <p></p>
+                <h4>Así funciona:</h4>
+                <p></p>
+                <p></p>
+                <a href="">
+                    <span>Presenta tu idea aquí</span>
+                </a>
+
+            </article>
+            <figure>
+                <img src="" alt="idea">
+            </figure>
+            <figure>
+                <img src="" alt="conoce el proceso  ">
+            </figure>
+            <article>
+                <p></p>
+            </article>
+            <div>
+                <a href="">
+                    <span>Conoce el proceso completo aquí</span>
+                </a>
+                <figure>
+                    <img src="" alt=" ">
+                </figure>
+            </div>
+        </section>
+        <section id="company">
+            <figure>
+                <img src="" alt="Para empresas">
+            </figure>
+            <p></p>
+            <param name="" value="">
+        </section>
+
+    </body>
 
        
     <!-- JavaScript -->
